c7ecd8e34896486c8013f2141613406f8e2bdd30
Emmanuelle B <128315974+EmmanuelleBonoli@users.noreply.github.com>
2023-10-06 13:32:54 +0200

    Update activitiesCardGeneration.js

--- activitiesCardGeneration.js
+++ activitiesCardGeneration.js
@@ -37,7 +37,7 @@ const listingActivities = [
         desc: "La boxe est un sport dynamique qui présente des atouts pour la forme et le mental. Elle permet d'améliorer le cardio vasculaire, l'endurance et la respiration. La boxe fait par conséquent travailler la respiration, l'endurance, l'équilibre et la force.",
         price1: "30 €",
         price2: "250 €",
-        difficulty: "images/haltereDiff3.PNG",
+        difficulty: "images/haltereDiff3.png",
         activityImg: "images/activityBox.jpg",
         link: "coach.html",
         data: "box"
@@ -47,7 +47,7 @@ const listingActivities = [
         desc: "Le cardio training est une forme d'activité physique rythmique qui permet d'augmenter votre fréquence cardiaque dans votre zone cible de fréquence cardiaque. C'est dans cette zone que votre corps brûle le plus de graisses et de calories.",
         price1: "30 €",
         price2: "250 €",
-        difficulty: "images/haltereDiff3.PNG",
+        difficulty: "images/haltereDiff3.png",
         activityImg: "images/activityCardio.jpg",
         link: "coach.html",
         data: "cardio"
@@ -57,7 +57,7 @@ const listingActivities = [
         desc: "La musculation vise au développement des muscles squelettiques, pratiquée dans le but d'acquérir plus de force, d'endurance ou de volume musculaire. Les bienfaits sont notables, un bon régime d'entrainement physique a été prouvé comme étant bénéfique pour la santé physique et mentale.",
         price1: "30 €",
         price2: "275 €",
-        difficulty: "images/haltereDiff2.PNG",
+        difficulty: "images/haltereDiff2.png",
         activityImg: "images/activityMusculation.jpg",
         link: "coach.html",
         data: "musculation"
@@ -67,7 +67,7 @@ const listingActivities = [
         desc: "Le yoga est une pratique qui nous vient de l'Inde et qui apporte un équilibre entre le corps, le mental et le spirituel.Le yoga permet d'améliorer sa force, sa souplesse et son équilibre en reforçant les muscles profonds. Lorsqu'il est pratiqué régulièrment il permet d'améliorer sa santé et sa vitalité.",
         price1: "30 €",
         price2: "250 €",
-        difficulty: "images/haltereDiff.PNG",
+        difficulty: "images/haltereDiff.png",
         activityImg: "images/activtyYoga.jpg",
         link: "coach.html",
         data: "yoga"
@@ -77,7 +77,7 @@ const listingActivities = [
         desc: "L'aquagym est une forme de gymnastique pratiquée dans l'eau. Les exercices sont effectués dans l'eau, dont la hauteur est choisie en fonction des exercices à pratiquer et de la résistance souhaitée. La pression de l'eau évite les chocs et minimise le risque de courbartures, de claquages ou d'élongations musculaires.",
         price1: "30€",
         price2: "250€",
-        difficulty: "images/haltereDiff.PNG",
+        difficulty: "images/haltereDiff.png",
         activityImg: "images/activity_AquaGym.jpg",
         link: "coach.html",
         data: "aquagym"
@@ -87,7 +87,7 @@ const listingActivities = [
         desc: "L'aquabike est pratiqué en piscine. Cela consiste à pédaler dans l'eau sur un vélo sans roue.En plus d'être une activité faisant travailler le cœur et brûlant les calories et les graisses en priorité, le fait de pratiquer dans l'eau permet de travailler en douceur.",
         price1: "50€",
         price2: "450€",
-        difficulty: "images/haltereDiff.PNG",
+        difficulty: "images/haltereDiff.png",
         activityImg: "images/activityAquaBike.jpg",
         link: "coach.html",
         data: "aquabike"
